4f78767071aec0be86926ad560c985629ceb7caa
Neevash Ramdial <mail@neevash.dev>
2020-07-15 20:58:00 -0400

    clean up root file

--- lib/embedly_preview.dart
+++ lib/embedly_preview.dart
@@ -1,18 +1,7 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+library embedly_preview;
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class EmbedlyPreview {
-  static const MethodChannel _channel =
-      const MethodChannel('embedly_preview');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export './models/embedly_response_model.dart';
+export './widgets/card_section.dart';
+export './widgets/image_preview.dart';
+export './widgets/rich_preview.dart';
+export './widgets/oembed_widget.dart';
