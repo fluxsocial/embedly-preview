22e7eda221bf9ab377a22e1d8813b711c73be557
Neevash Ramdial <mail@neevash.dev>
2020-08-11 23:43:59 -0400

    Add new strings

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,6 @@
 class Strings {
   static const sharedBy = "Shared by";
   static const description = "Description";
+  static const unSupported = "Unsupported Link Type";
+  static const comingSoon = "Coming Soon";
 }
