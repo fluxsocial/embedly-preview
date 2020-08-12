29b4c5f8dfbcff70b2ece242c68ab17e23beb071
Neevash Ramdial <mail@neevash.dev>
2020-08-11 23:44:11 -0400

    deprecate image_preview.dart

--- lib/embedly_preview.dart
+++ lib/embedly_preview.dart
@@ -2,6 +2,5 @@ library embedly_preview;
 
 export './models/embedly_response_model.dart';
 export './widgets/card_section.dart';
-export './widgets/image_preview.dart';
 export './widgets/rich_preview.dart';
 export './widgets/oembed_widget.dart';
